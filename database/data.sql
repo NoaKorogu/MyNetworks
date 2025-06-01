0a0441cc8bae17373019bbad6b47a7508ea6e114
Noa <161039284+NoaKorogu@users.noreply.github.com>
2025-06-01 10:58:47 +0200

    Create data.sql

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO network(name) VALUES("Electricité");
+INSERT INTO network(name) VALUES("Eau");
+INSERT INTO network(name) VALUES("Bus");
+INSERT INTO network(name) VALUES("Admin");
+INSERT INTO type (name, network_id) VALUES ("Arrêt de bus",3);
+INSERT INTO type (name, network_id) VALUES ("Poteau haute tension",1);
+INSERT INTO type (name, network_id) VALUES ("Fontaine",2);

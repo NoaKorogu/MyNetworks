b6f85d20c457dadf3e5b3e1fb80e441f454bbe98
Kaneko <58117418+KAN3KO@users.noreply.github.com>
2025-06-01 21:30:33 +0200

    Update data.sql

--- database/data.sql
+++ database/data.sql
@@ -3,5 +3,8 @@ INSERT INTO network(name) VALUES("Eau");
 INSERT INTO network(name) VALUES("Bus");
 INSERT INTO network(name) VALUES("Admin");
 INSERT INTO type (name, network_id) VALUES ("Arrêt de bus",3);
+INSERT INTO type (name, network_id) VALUES ("Dépôt de bus",3);
 INSERT INTO type (name, network_id) VALUES ("Poteau haute tension",1);
+INSERT INTO type (name, network_id) VALUES ("Central éléctrique",1);
 INSERT INTO type (name, network_id) VALUES ("Fontaine",2);
+INSERT INTO type (name, network_id) VALUES ("Château d'eau",2);
